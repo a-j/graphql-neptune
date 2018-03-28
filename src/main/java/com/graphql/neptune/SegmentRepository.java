4eb38ac20ffc0460ef5983b3328148c432e91a93
Anand Jayabalan <ajayabalan@sapient.com>
2018-03-28 12:59:25 -0400

    added error handling code better handle errors

--- src/main/java/com/graphql/neptune/SegmentRepository.java
+++ src/main/java/com/graphql/neptune/SegmentRepository.java
@@ -32,25 +32,34 @@ public class SegmentRepository {
     public List<Segment> getSegments(String customerId) {
         List<Segment> segments = new ArrayList<>();
 
-        List<Map<Object, Object>> valueMap = g.V().hasId(customerId).out("PART_OF").valueMap(true).toList();
+        try {
+            List<Map<Object, Object>> valueMap = g.V().hasId(customerId).out("PART_OF").valueMap(true).toList();
 
-        for (Map map : valueMap) {
-            segments.add(new Segment(map.get(T.id).toString(), ((List) map.get("name")).get(0).toString(), null, null));
+            for (Map map : valueMap) {
+                segments.add(new Segment(map.get(T.id).toString(), ((List) map.get("name")).get(0).toString(), null, null));
+            }
+        } catch (Exception e) {
+            logger.error("Segments not found for user {}", customerId, e);
         }
 
         return segments;
     }
 
     public Profile getProfile(String customerId) {
-        Map<Object, Object> map = g.V().hasId(customerId).out("HAS_PROFILE").valueMap(true).next();
-
         Profile profile = new Profile();
-        profile.setId(map.get(T.id).toString());
-        profile.setAge(Integer.valueOf(((List) map.get("age")).get(0).toString()));
-        profile.setAddress(((List) map.get("address")).get(0).toString());
-        profile.setCity(((List) map.get("city")).get(0).toString());
-        profile.setState(((List) map.get("state")).get(0).toString());
-        profile.setZipcode(((List) map.get("zipcode")).get(0).toString());
+
+        try {
+            Map<Object, Object> map = g.V().hasId(customerId).out("HAS_PROFILE").valueMap(true).next();
+
+            profile.setId(map.get(T.id).toString());
+            profile.setAge(Integer.valueOf(((List) map.get("age")).get(0).toString()));
+            profile.setAddress(((List) map.get("address")).get(0).toString());
+            profile.setCity(((List) map.get("city")).get(0).toString());
+            profile.setState(((List) map.get("state")).get(0).toString());
+            profile.setZipcode(((List) map.get("zipcode")).get(0).toString());
+        } catch (Exception e) {
+            logger.error("Profile not found for user {}", customerId, e);
+        }
 
         return profile;
     }
