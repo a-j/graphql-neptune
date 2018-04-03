47337ca55c47a430518fcb1327c74fe8d58b3557
Anand Jayabalan <ajayabalan@sapient.com>
2018-04-03 14:57:02 -0500

    use tryNext() instead of next()

--- src/main/java/com/graphql/neptune/SegmentRepository.java
+++ src/main/java/com/graphql/neptune/SegmentRepository.java
@@ -102,7 +102,7 @@ public class SegmentRepository {
             }
             logger.info("Customer Id: {} | Segment Ids: {}", customerId, segmentIds.toString());
 
-            g.V().hasId(customerId).outE("PART_OF").drop().next();
+            g.V().hasId(customerId).outE("PART_OF").drop().tryNext();
             logger.info("Edges for customer id {} dropped", customerId);
 
             for (String segmentId : segmentIds) {
